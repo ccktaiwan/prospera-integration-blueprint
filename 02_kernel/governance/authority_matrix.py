e6a455a0dd9ef8584a54a05fc9b0948474bb70f1
Prospera Governance Engine <governance@ccktaiwan.org>
2026-05-18 13:54:20 +0800

    [P1][L4] feat: add 02_kernel authority matrix + registry (Scanner L4 gate)

---
+++ 02_kernel/governance/authority_matrix.py
@@ -0,0 +1,10 @@
+﻿"""
+Prospera Authority Matrix
+Repo: prospera-integration-blueprint
+Version: v1.0
+"""
+AUTHORITY_MATRIX = {
+    "owner": "ccktaiwan",
+    "governed_by": "prospera-engineering-codex v1.0",
+    "ci_required": True,
+}
---
+++ 02_kernel/registry/__init__.py
@@ -0,0 +1,2 @@
+﻿# Registry loader for prospera-integration-blueprint
+REGISTRY = {}
